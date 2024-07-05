b2b44135ed9bd66615a95326edb891406a1d1edb
yongwoon <yonyon.japan@gmail.com>
2024-07-05 10:26:13 +0900

    不要なファイル削除 && Setup Gql module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // https://docs.nestjs.com/graphql/quick-start#code-first
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true, // true: Browser から GraphQL Playground にアクセスできる
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      // autoSchemaFile: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
