e5fb9f8f2b982f97686446cc22d4377cc9d14d84
yongwoon <yonyon.japan@gmail.com>
2024-07-05 10:28:49 +0900

    ADD: UserResolver.isUser

--- backend/src/users/users.resolver.ts
+++ backend/src/users/users.resolver.ts
@@ -1,4 +1,9 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
-export class UsersResolver {}
+export class UsersResolver {
+  @Query(() => Boolean)
+  isUser(): boolean {
+    return true;
+  }
+}
